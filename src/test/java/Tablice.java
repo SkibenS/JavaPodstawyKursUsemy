2c816565aa65a9b3704e468f5c247ec8e130febc
Your Name <you@example.com>
2020-04-16 10:47:56 +0200

    Usunięcie pliku

---
+++ src/test/java/Tablice.java
@@ -0,0 +1,12 @@
+public class Tablice {
+
+    int liczby[] = new int[5];
+
+    public void ustawWastosc() {
+        liczby[0] = 1;
+        liczby[1] = 10;
+        liczby[2] = 25;
+        liczby[3] = 100;
+        liczby[4] = 150;
+    }
+}
