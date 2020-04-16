af0bbe9cd4462e6212cdf6ae3133db77c9eb65ec
Your Name <you@example.com>
2020-04-16 12:24:51 +0200

    Tablice commit

--- src/test/java/Tablice.java
+++ src/test/java/Tablice.java
@@ -13,8 +13,14 @@ public class Tablice {
     }
 
     public void przedstawStudenta() {
-        studenci[0].przedstawSie();
-        studenci[1].przedstawSie();
-
+        try {studenci[0].przedstawSie();
+            studenci[1].przedstawSie();
+           // studenci[2].przedstawSie();
+            System.out.println("Tutaj znajduje się coś innego");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Wystąpił wyjątek ! Odwołujesz się do elementu tablicy którego nie ma");
+        } finally {
+            System.out.println("Zamykam połaczenie do bazy danych");
+        }
     }
 }
